6e705513c1a113776e8019f04f38b007783541b7
SivaVaishnavi <vaishnavipathivada32@gmail.com>
2026-03-02 11:59:50 +0530

    automate testing

--- src/test/java/com/example/demo/CseControllerTest.java
+++ src/test/java/com/example/demo/CseControllerTest.java
@@ -2,17 +2,14 @@ package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CseControllerTest {
-	@Autowired
-	CseController c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
+public class CseControllerTest {
 
+    @Test
+    public void testAddCSE() {
+        CseController controller = new CseController();
+        String result = controller.addCSE(10, 5);
+        String expected = "The value is 15 and  ROLL NUMBER is 24P35A0550";
+        assertEquals(expected, result);
+    }
 }
